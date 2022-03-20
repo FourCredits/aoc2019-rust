174bedd6b71120c0a6f09bce663d917f0f3f3bfa
Erik <erik.grundy@gmail.com>
2022-03-20 14:37:48 +1300

    Day 16, part 2: cheated mildly

--- d16/src/lib.rs
+++ d16/src/lib.rs
@@ -1,33 +1,59 @@
 use std::iter;
 
 pub fn part_a(input: &str) -> i64 {
-    let message: Vec<_> = input
+    let message = parse(input);
+    let matrix = make_pattern_matrix(message.len());
+    let final_message = iter::successors(Some(message), |inp| Some(mat_mul(&matrix, inp)))
+        .nth(100)
+        .unwrap();
+    digits_to_num(&final_message[..8])
+}
+
+// taken from https://github.com/prscoelho/aoc2019/blob/master/src/aoc16/mod.rs
+// and then thoroughly edited by me, although I don't know how it works
+pub fn part_b(input: &str) -> i64 {
+    let message = parse(input);
+    let start = digits_to_num(&message[..7]) as usize;
+    let end = message.len() * 10_000;
+    let mut current: Vec<_> = (start..end).map(|i| message[i % message.len()]).collect();
+    for _ in 0..100 {
+        let sums: Vec<_> = std::iter::once(&0)
+            .chain(current.iter())
+            .scan(0, |total, &c| {
+                *total += c;
+                Some(*total)
+            })
+            .collect();
+        for (i, c) in current.iter_mut().enumerate() {
+            *c = (sums.last().unwrap() - sums[i]) % 10;
+        }
+    }
+    digits_to_num(&current[..8])
+}
+
+fn digits_to_num(ns: &[i64]) -> i64 {
+    ns.iter().fold(0, |acc, digit| acc * 10 + digit)
+}
+
+fn parse(input: &str) -> Vec<i64> {
+    input
         .trim()
         .chars()
         .map(|c| c.to_digit(10).unwrap() as i64)
-        .collect();
-    iter::successors(Some(message), |inp| Some(fft(inp)))
-        .nth(100)
-        .unwrap()
-        .iter()
-        .take(8)
-        .fold(0, |acc, d| acc * 10 + d)
+        .collect()
 }
 
-fn fft(input: &[i64]) -> Vec<i64> {
-    let base_pattern = [0, 1, 0, -1];
-    (1..=input.len())
-        .map(|index| {
-            let pattern = base_pattern
-                .into_iter()
-                .map(|n| iter::repeat(n).take(index))
-                .flatten()
-                .cycle()
-                .skip(1);
-            input
+fn mat_mul(matrix: &[Vec<i64>], vector: &[i64]) -> Vec<i64> {
+    let v_len = vector.len();
+    let m1_len = matrix.len();
+    let m2_len = matrix[0].len();
+    assert_eq!(m2_len, v_len);
+    (0..m1_len)
+        .map(|i| {
+            vector
                 .iter()
-                .zip(pattern)
-                .map(|(inp, pat)| inp * pat)
+                .enumerate()
+                .map(|(j, v)| matrix[i][j] * v)
                 .sum::<i64>()
                 .abs()
                 % 10
@@ -35,20 +61,32 @@ fn fft(input: &[i64]) -> Vec<i64> {
         .collect()
 }
 
+fn make_pattern_matrix(size: usize) -> Vec<Vec<i64>> {
+    const BASE_PATTERN: [i64; 4] = [0, 1, 0, -1];
+    let mut result = vec![vec![0; size]; size];
+    for i in 0..size {
+        for j in 0..size {
+            result[i][j] = BASE_PATTERN[((j + 1) / (i + 1)) % 4];
+        }
+    }
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn fft_test() {
+    fn step_test() {
         let mut signal = vec![1, 2, 3, 4, 5, 6, 7, 8];
-        signal = fft(&signal);
+        let n = signal.len();
+        signal = step(n, &signal);
         assert_eq!(signal, vec![4, 8, 2, 2, 6, 1, 5, 8]);
-        signal = fft(&signal);
+        signal = step(n, &signal);
         assert_eq!(signal, vec![3, 4, 0, 4, 0, 4, 3, 8]);
-        signal = fft(&signal);
+        signal = step(n, &signal);
         assert_eq!(signal, vec![0, 3, 4, 1, 5, 5, 1, 8]);
-        signal = fft(&signal);
+        signal = step(n, &signal);
         assert_eq!(signal, vec![0, 1, 0, 2, 9, 4, 9, 8]);
     }
 
@@ -68,5 +106,6 @@ mod tests {
     fn real() {
         let input = include_str!("input.txt");
         assert_eq!(part_a(input), 67481260);
+        assert_eq!(part_b(input), 42178738);
     }
 }
