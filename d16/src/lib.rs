e8779babb2eeca1efbe5197f4922243b3b533564
Erik <erik.grundy@gmail.com>
2022-03-20 14:42:09 +1300

    Remove defunct test

--- d16/src/lib.rs
+++ d16/src/lib.rs
@@ -77,20 +77,6 @@ mod tests {
     use super::*;
 
     #[test]
-    fn step_test() {
-        let mut signal = vec![1, 2, 3, 4, 5, 6, 7, 8];
-        let n = signal.len();
-        signal = step(n, &signal);
-        assert_eq!(signal, vec![4, 8, 2, 2, 6, 1, 5, 8]);
-        signal = step(n, &signal);
-        assert_eq!(signal, vec![3, 4, 0, 4, 0, 4, 3, 8]);
-        signal = step(n, &signal);
-        assert_eq!(signal, vec![0, 3, 4, 1, 5, 5, 1, 8]);
-        signal = step(n, &signal);
-        assert_eq!(signal, vec![0, 1, 0, 2, 9, 4, 9, 8]);
-    }
-
-    #[test]
     fn part_a_example() {
         let inputs = [
             ("80871224585914546619083218645595", 24176176),
